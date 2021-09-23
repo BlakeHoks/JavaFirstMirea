20e2cd39e8a95bbbe720c99ffce3c697c1f4ebb4
IVC1-5 <asd>
2021-09-23 14:08:34 +0300

    Started task 10

---
+++ src/ru/mirea/task10/Functions.java
@@ -0,0 +1,35 @@
+package ru.mirea.task10;
+
+import javax.swing.plaf.synth.SynthLookAndFeel;
+import java.util.Scanner;
+
+public class Functions {
+    public static void DigitsFromRightToLeft(int n){
+        if (n >= 10) {
+            System.out.println(n % 10);
+            n /= 10;
+            DigitsFromRightToLeft(n);
+        } else {
+            System.out.println(n);
+        }
+    }
+
+    public static void NumOfMaxElems(){
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        int[] arr = new int[3];
+        int i = 0;
+        while (num != 0) {
+            num = in.nextInt();
+            arr[i] = num;
+            i++;
+        }
+        System.out.println(arr);
+
+    }
+
+    public static void main(String[] args){
+        //DigitsFromRightToLeft(1236128);
+        NumOfMaxElems();
+    }
+}
